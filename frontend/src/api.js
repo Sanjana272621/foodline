e5774607a4a29b274a9f06c248598cae386704ae
Sanjana272621 <sanjana.sureshkumar05@gmail.com>
2025-04-06 14:00:12 +0530

    changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -15,22 +15,23 @@ api.interceptors.request.use(config => {
 });
 
 export const login = async (email, password) => {
-  const formData = new URLSearchParams();
-  formData.append('username', email);
-  formData.append('password', password);
+    const formData = new URLSearchParams();
+    formData.append('username', email);
+    formData.append('password', password);
   
-  const response = await api.post('/users/token', formData, {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  });
-  return response.data;
-};
-
-export const register = async (userData) => {
-  const response = await api.post('/users/register', userData);
-  return response.data;
-};
+    const response = await api.post('/users/token', formData, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+  
+    return response.data;
+  };
+  
+  export const register = async (userData) => {
+    const response = await api.post('/users/register', userData);
+    return response.data;
+  };
 
 export const getUserProfile = async () => {
   const response = await api.get('/users/me');
